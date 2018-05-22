37459f8109e2dd4aa35692ba5b8a6e4f63d42823
Cecilia <ceciliapigeolet@gmail.com>
2018-05-22 10:02:04 +0200

    first commit

---
+++ src/main/java/com/cess/SpringReseau/SpringReseauApplication.java
@@ -0,0 +1,12 @@
+package com.cess.SpringReseau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReseauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReseauApplication.class, args);
+	}
+}
